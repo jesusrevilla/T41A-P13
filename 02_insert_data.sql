c3a7a5ca135233e0bc922f993cec5296c696a287
patymtz17 <181730@upslp.edu.mx>
2025-10-28 19:28:53 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,1 +1,13 @@
 
+\copy lista_temp FROM 'lista.csv' DELIMITER ',' CSV HEADER;
+
+INSERT INTO lista (matricula, apellido, equipo)
+SELECT 
+    matricula,
+    apellido,
+    CASE 
+        WHEN matricula IN (179800,181730,179752,183060,182483,178974,177622) 
+        THEN 'Cachorritas'
+        ELSE 'Otros'
+    END AS equipo
+FROM lista_temp;
