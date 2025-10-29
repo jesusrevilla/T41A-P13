d3e9e849285558fdb7727f86e247decab86b40fb
Bryan Yareth Gámez Contreras <96131248+BryanYarethGC@users.noreply.github.com>
2025-10-28 19:44:13 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -5,7 +5,7 @@ INSERT INTO empleados (nombre, departamento, salario) VALUES
 ('Carlos', 'Ventas', 55000),
 ('Elena', 'RRHH', 48000);
 
-\COPY alumnos from 'lista.csv' delimiter ',' CSV HEADER;
+\copy alumnos(matricula, apellido) FROM 'lista.csv' WITH (FORMAT CSV, HEADER TRUE);
 
 INSERT INTO equipos values (1,'QUESOEXTRA');
 
