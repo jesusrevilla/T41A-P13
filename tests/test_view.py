62e4194498e718a0e1eccacf45c6f18743f1d1a0
Bryan Yareth Gámez Contreras <96131248+BryanYarethGC@users.noreply.github.com>
2025-10-28 19:31:44 -0600

    Update test_view.py

--- tests/test_view.py
+++ tests/test_view.py
@@ -17,3 +17,24 @@ def test_vista_ti():
     assert 'María' in nombres
     cur.close()
     conn.close()
+
+def test_vista_equipo():
+    conn = psycopg2.connect(
+        dbname='test_db',
+        user='postgres',
+        password='postgres',
+        host='localhost',
+        port='5432'
+    )
+    cur = conn.cursor()
+    cur.execute(\"SELECT * FROM vista_equipo;\")
+    resultados = cur.fetchall()
+    matriculas = [fila[0] for fila in resultados]
+    assert '182934' in matriculas
+    assert '182451' in matriculas
+    assert '182829' in matriculas
+    assert '179761' in matriculas
+    assert '181914' in matriculas
+    assert '182085' in matriculas
+    cur.close()
+    conn.close()
