7fc50845870b378d41c3cc7f9c5164c0f66f3642
Rafael López <126181633+Refex38@users.noreply.github.com>
2025-10-28 19:19:35 -0600

    Create empleados and alumnos tables

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,1 +1,11 @@
+CREATE TABLE empleados (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100),
+    departamento VARCHAR(50),
+    salario NUMERIC
+);
 
+CREATE TABLE alumnos(
+  matricula INTEGER PRIMARY KEY,
+  apellido VARCHAR(20)
+);
