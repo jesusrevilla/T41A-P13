7970c4abe4ce7e90e6f6a802d684cc702c5192b9
patymtz17 <181730@upslp.edu.mx>
2025-10-28 21:47:34 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,18 +1,15 @@
--- Crear tabla lista
-CREATE TABLE IF NOT EXISTS lista (
+CREATE TABLE lista (
     matricula INT PRIMARY KEY,
     apellido VARCHAR(100),
     equipo VARCHAR(50)
 );
 
--- Tabla temporal para importar CSV
-CREATE TEMP TABLE lista_temp (
+CREATE TABLE lista_temp (
     matricula INT,
     apellido VARCHAR(100)
 );
 
--- Crear tabla empleados
-CREATE TABLE IF NOT EXISTS empleados (
+CREATE TABLE empleados (
     nombre VARCHAR(100),
     departamento VARCHAR(50),
     salario NUMERIC
