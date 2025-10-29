092b78c8a0d2a2697508992579e92e1a0cb85310
YaelQCI <181914@upslp.edu.mx>
2025-10-28 19:51:26 -0600

    Update test_view.py

--- tests/test_view.py
+++ tests/test_view.py
@@ -17,3 +17,25 @@ def test_vista_ti():
     assert 'María' in nombres
     cur.close()
     conn.close()
+
+def test_vista_equipo():
+    conn = psycopg2.connect(
+        dbname='test_db',
+        user='postgres',
+        password='postgres',
+        host='localhost',
+        port='5432'
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM vista_equipo;")
+    resultados = cur.fetchall()
+    assert len(resultados) == 6  # Solo Luis y María están en TI
+    nombres = [fila[0] for fila in resultados]
+    assert 'Quintanilla' in nombres
+    assert 'Domínguez' in nombres
+    assert 'Gámez' in nombres
+    assert 'García' in nombres
+    assert 'Martínez' in nombres
+    assert 'Ornelas' in nombres
+    cur.close()
+    conn.close()
