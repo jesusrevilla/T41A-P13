6576c7edb01f05f824ba9babd33f586c46f1ab5c
patymtz17 <181730@upslp.edu.mx>
2025-10-28 22:21:31 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,19 +1,20 @@
+
+\copy lista_temp FROM 'lista.csv' DELIMITER ',' CSV HEADER;
+
+INSERT INTO lista (matricula, apellido, equipo)
+SELECT 
+    matricula,
+    apellido,
+    CASE 
+        WHEN matricula IN (179800,181730,179752,183060,182483,178974,177622)
+        THEN 'Cachorritas'
+        ELSE 'Otros'
+    END AS equipo
+FROM lista_temp;
+
 INSERT INTO empleados (nombre, departamento, salario) VALUES
-('Ana', 'Ventas', 50000),
 ('Luis', 'TI', 60000),
 ('María', 'TI', 65000),
+('Ana', 'Ventas', 50000),
 ('Carlos', 'Ventas', 55000),
 ('Elena', 'RRHH', 48000);
-
-\copy alumnos(matricula, apellido) FROM 'lista.csv' WITH (FORMAT CSV, HEADER TRUE);
-
-INSERT INTO equipos values (1,'Cachorritas');
-
-INSERT INTO alumno_equipo values
-('177622',1),
-('178974',1),
-('179752',1),
-('179800',1),
-('181730',1),
-('182483',1);
-('183060',1);
