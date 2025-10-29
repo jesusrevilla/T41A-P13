87d6c2513492106afbf9799a9e6a2a0acb48cf5d
patymtz17 <181730@upslp.edu.mx>
2025-10-28 22:15:34 -0600

    Update 03_create_views.sql

--- 03_create_views.sql
+++ 03_create_views.sql
@@ -1,12 +1,9 @@
 CREATE VIEW vista_ti AS
-SELECT nombre,departamento
+SELECT nombre
 FROM empleados
 WHERE departamento='TI';
 
-
-CREATE VIEW cachorritas AS
-SELECT matricula, apellido
-FROM lista
-WHERE matricula==182483||matricula==179752||matricula==183060
-||matricula==181730||matricula==183213||matricula==182712
-||matricula==179800;
+CREATE VIEW vista_equipo AS
+SELECT *
+FROM alumno_equipo
+WHERE equipo=1;
