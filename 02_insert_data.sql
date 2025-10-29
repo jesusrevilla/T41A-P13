b553128f124b3e93341f7eb27cbce6ebf6841bba
Jpablo179804 <179804@upslp.edu.mx>
2025-10-28 23:07:32 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -7,4 +7,5 @@ INSERT INTO empleados (nombre, departamento, salario) VALUES
 
 
 --\COPY compañeros (matricula, apellido) FROM 'lista.csv' WITH (FORMAT CSV, HEADER TRUE, DELIMITER ',';
-COPY compañeros (matricula, apellido) FROM '/lista.csv' DELIMITER ',' CSV HEADER;
+--COPY compañeros (matricula, apellido) FROM '/lista.csv' DELIMITER ',' CSV HEADER;
+\copy compañeros FROM 'lista.csv' DELIMITER ',' CSV HEADER;
