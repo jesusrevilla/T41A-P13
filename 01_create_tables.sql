532f93f98f3721ebb4872e1fe0fefc503858349b
Bryan Yareth Gámez Contreras <96131248+BryanYarethGC@users.noreply.github.com>
2025-10-28 19:21:00 -0600

    Create tables for empleados, equipos, and alumnos

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,1 +1,18 @@
+CREATE TABLE empleados (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100),
+    departamento VARCHAR(50),
+    salario NUMERIC
+);
 
+CREATE TABLE equipos(
+  id_equipo INT PRIMARY KEY,
+  nombre_equipo VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE alumnos(
+  matricula VARCHAR(6) PRIMARY KEY,
+  apellido VARCHAR(100) NOT NULL,
+  nombre VARCHAR(100) null,
+  equipo INT REFERENCES equipos(id_equipo) null
+);
