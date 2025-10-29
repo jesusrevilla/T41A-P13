030fc8826ace9a93d844dd318e4cc5af9d81b571
Rafael López <126181633+Refex38@users.noreply.github.com>
2025-10-28 19:27:36 -0600

    Add test for vista_matriculas function

--- tests/test_view.py
+++ tests/test_view.py
@@ -17,3 +17,21 @@ def test_vista_ti():
     assert 'María' in nombres
     cur.close()
     conn.close()
+
+def test_vista_matriculas():
+    conn = psycopg2.connect(
+        dbname='test_db',
+        user='postgres',
+        password='postgres',
+        host='localhost',
+        port='5432'
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM vista_matriculas;")
+    resultados = cur.fetchall()
+    assert len(resultados) == 2  
+    matriculas = [fila[0] for fila in resultados]
+    assert '182239' in matricuals
+    assert '179804' in matriculas
+    cur.close()
+    conn.close()
