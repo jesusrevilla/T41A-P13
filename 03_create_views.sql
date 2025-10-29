7e55fc2fc8274e0f163456585b3a4d5122619b12
Palau003 <178561@upslp.edu.mx>
2025-10-28 19:30:45 -0600

    Update 03_create_views.sql

--- 03_create_views.sql
+++ 03_create_views.sql
@@ -1,1 +1,5 @@
-
+-- Crear vista para mostrar solo los compañeros de tu equipo
+CREATE VIEW vista_equipo AS
+SELECT matricula, apellido
+FROM companeros
+WHERE apellido IN ('Palau', 'Ibarra', 'García', 'Saucedo');
