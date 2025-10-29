fa89a89c9b61588f6e072e4a6fcf7351d7beb882
YaelQCI <181914@upslp.edu.mx>
2025-10-28 19:47:24 -0600

    Update 03_create_views.sql

--- 03_create_views.sql
+++ 03_create_views.sql
@@ -1,1 +1,7 @@
 CREATE VIEW vista_ti AS SELECT * FROM empleados WHERE departamento='TI';
+
+CREATE VIEW equipo AS
+SELECT a.matricula, a.apellido 
+FROM integrantesEquipos i 
+JOIN alumnos a 
+ON i.matricula_alumno = a.matricula; 
