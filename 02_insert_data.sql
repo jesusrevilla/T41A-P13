49e11bf74d54ae8829ee969427c1e4d657bb6084
Uriel4443 <182829@upslp.edu.mx>
2025-10-28 21:58:27 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -2,7 +2,7 @@
 
 UPDATE equipo
 SET team  = '1'
-WHERE matricula IN ('182829', '182451', '182934', '179761');  
+WHERE Matrícula IN ('182829', '182451', '182934', '179761');  
 INSERT INTO empleados (nombre, departamento, salario) VALUES
 ('Ana', 'Ventas', 50000),
 ('Luis', 'TI', 60000),
