751c241bcb086432f5ab774d95f5b66206c808a6
patymtz17 <181730@upslp.edu.mx>
2025-10-28 19:28:37 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,6 +1,11 @@
+
 CREATE TABLE lista (
     matricula INT PRIMARY KEY,
     apellido VARCHAR(100),
     equipo VARCHAR(50)
 );
 
+CREATE TEMP TABLE lista_temp (
+    matricula INT,
+    apellido VARCHAR(100)
+);
