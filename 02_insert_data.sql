3a918afdbafa51f932285c80a1c0ee15195c2c8b
menaxixi <179800@upslp.edu.mx>
2025-10-28 19:42:43 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,1 +1,10 @@
+\copy lista (matricula, apellido) FROM 'lista.csv' DELIMITER ',' CSV HEADER;
 
+INSERT INTO equipo_cachorritas (matricula)
+VALUES 
+(179752),
+(177622),
+(182483),
+(179800),
+(183060)
+ON CONFLICT (matricula) DO NOTHING;
