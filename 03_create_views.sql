2ffbc117540dc992d6206b3a6df287e0d4d74e45
patymtz17 <181730@upslp.edu.mx>
2025-10-28 21:41:07 -0600

    Update 03_create_views.sql

--- 03_create_views.sql
+++ 03_create_views.sql
@@ -1,9 +1,11 @@
-CREATE VIEW vista_ti AS
-SELECT columnas
-FROM tabla
-WHERE condiciones;
-
+-- Crear vista del equipo Cachorritas
 CREATE OR REPLACE VIEW vista_cachorritas AS
 SELECT matricula, apellido
 FROM lista
 WHERE equipo = 'Cachorritas';
+
+-- Crear vista TI para los tests
+CREATE OR REPLACE VIEW vista_ti AS
+SELECT nombre, departamento, salario
+FROM empleados
+WHERE departamento = 'TI';
