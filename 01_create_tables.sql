65c80f4ba1f140c702d50ed2413e2d9d28ec758d
ItsMusa17 <160455957+ItsMusa17@users.noreply.github.com>
2025-10-28 19:26:23 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,1 +1,9 @@
+-- Tabla para los integrantes del equipo
+CREATE TABLE equipo (
+    matricula VARCHAR(10) PRIMARY KEY,
+    nombre VARCHAR(50),
+    apellido_paterno VARCHAR(50),
+    apellido_materno VARCHAR(50),
+    grupo VARCHAR(10)
+);
 
