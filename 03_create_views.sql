7179c1f0bbdf66f3aff6644e5bb27563b7e1e7c4
Cristphertorres21 <cristophertorresdza@gmail.com>
2025-10-28 19:50:03 -0600

    Update 03_create_views.sql

--- 03_create_views.sql
+++ 03_create_views.sql
@@ -1,7 +1,4 @@
-CREATE VIEW vista_equipo AS
--- Crear vista solo con los compañeros del equipo (por matrícula específica)
 CREATE OR REPLACE VIEW vista_equipo AS
 SELECT matricula, apellido
 FROM companeros
-WHERE apellido IN ('Palau', 'Ibarra', 'García', 'Saucedo');
 WHERE matricula IN (178561, 176453, 178666, 178974);
