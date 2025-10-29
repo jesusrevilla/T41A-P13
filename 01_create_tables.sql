fb321b6c645cb577ae46dcac401996b611345f5d
menaxixi <179800@upslp.edu.mx>
2025-10-28 19:47:54 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,10 +1,16 @@
-CREATE TABLE lista (
+CREATE TABLE IF NOT EXISTS empleados (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100),
+    departamento VARCHAR(50),
+    salario NUMERIC
+);
+
+CREATE TABLE IF NOT EXISTS lista (
     matricula INT PRIMARY KEY,
     apellido VARCHAR(100)
 );
 
-CREATE TABLE equipo_cachorritas (
+CREATE TABLE IF NOT EXISTS equipo_cachorritas (
     matricula INT PRIMARY KEY,
     equipo VARCHAR(50) DEFAULT 'cachorritas'
 );
-
