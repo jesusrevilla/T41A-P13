4279b22153132eaad7fc8bb9fde2f7d13a78e8ff
menaxixi <179800@upslp.edu.mx>
2025-10-28 19:40:17 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,1 +1,10 @@
+CREATE TABLE lista (
+    matricula INT PRIMARY KEY,
+    apellido VARCHAR(100)
+);
+
+CREATE TABLE equipo_cachorritas (
+    matricula INT PRIMARY KEY,
+    equipo VARCHAR(50) DEFAULT 'cachorritas'
+);
 
