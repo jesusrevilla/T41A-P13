383dbfaccd10bd2303777060eb8f8b3c37115ee1
Yovana-Palencia <174197@upslp.edu.mx>
2025-10-28 19:44:10 -0600

    Update 03_create_views.sql

--- 03_create_views.sql
+++ 03_create_views.sql
@@ -1,1 +1,12 @@
+-- Vista para empleados del departamento de TI
+CREATE VIEW vista_ti AS
+SELECT id, nombre, salario
+FROM empleados
+WHERE departamento = 'TI';
 
+-- Vista PRINCIPAL para tus compañeros del Equipo A
+CREATE VIEW mi_equipo AS
+SELECT matricula, apellido
+FROM companeros
+WHERE equipo = 'Equipo A'
+ORDER BY apellido;
