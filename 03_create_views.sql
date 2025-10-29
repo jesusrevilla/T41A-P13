d056ab2a79e75d8675539b5e7d5f1cf182fdcba3
AdriannMo <165124296+AdriannMo@users.noreply.github.com>
2025-10-28 19:41:16 -0600

    Update 03_create_views.sql

--- 03_create_views.sql
+++ 03_create_views.sql
@@ -1,1 +1,12 @@
+-- 1. Vista para el test existente
+CREATE VIEW vista_ti AS
+SELECT nombre
+FROM empleados
+WHERE departamento = 'TI';
 
+
+-- 2. Vista para vista de equipo
+CREATE VIEW vista_equipo AS
+SELECT matricula, apellido
+FROM companeros
+WHERE matricula IN ('177139', '177700'); -- Filtro para los licenciados valeriano y siberiano
