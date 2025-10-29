585cd9af51a9580abfc58e43642d9e03eaec2a5e
rafaelSanchez4330 <rafaelsanchez9924@gmail.com>
2025-10-28 19:31:18 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,1 +1,14 @@
 
+CREATE TABLE lista(
+  matricula INTEGER PRIMARY KEY NOT NULL,
+  apellido VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE lista_equipos(
+  matricula INTEGER PRIMARY KEY NOT NULL,
+  apellido VARCHAR(30) NOT NULL,
+  nombre_equipo VARCHAR(25),
+  numero_equipo INTEGER
+);
+
+
