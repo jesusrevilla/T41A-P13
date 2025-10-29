246f94c1e8ec427b43ff9beec7eae512aa97b319
Jpablo179804 <179804@upslp.edu.mx>
2025-10-28 19:41:33 -0600

    Update 03_create_views.sql

--- 03_create_views.sql
+++ 03_create_views.sql
@@ -1,6 +1,10 @@
-CREATE VIEW vista_ti AS 
-SELECT id, nombre, departamento, salario
+--CREATE VIEW vista_ti AS 
+--SELECT id, nombre, departamento, salario
+--FROM empleados WHERE departamento = 'TI';
+CREATE OR REPLACE VIEW vista_ti AS
+SELECT nombre, id, departamento, salario
 FROM empleados WHERE departamento = 'TI';
+
 
 
 CREATE VIEW vista_mi_equipo AS
