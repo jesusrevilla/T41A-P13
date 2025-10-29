374891f428d4012d8c8f720851c57ec789e49d47
ItsMusa17 <160455957+ItsMusa17@users.noreply.github.com>
2025-10-28 19:26:44 -0600

    Update 03_create_views.sql

--- 03_create_views.sql
+++ 03_create_views.sql
@@ -1,1 +1,6 @@
+-- Vista para mostrar solo a los compañeros del equipo ITI
+CREATE OR REPLACE VIEW vista_equipo_iti AS
+SELECT matricula, nombre, apellido_paterno, apellido_materno
+FROM equipo
+WHERE grupo = 'ITI';
 
