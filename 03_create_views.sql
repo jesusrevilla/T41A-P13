41213a386f44ebb4b2d1a74cc9b102c12b3ea3a4
Cruz <183213@upslp.edu.mx>
2025-10-28 19:48:50 -0600

    Update 03_create_views.sql

--- 03_create_views.sql
+++ 03_create_views.sql
@@ -2,3 +2,11 @@ CREATE VIEW vista_ti AS
 SELECT nombre,departamento
 FROM empleados
 WHERE departamento='TI';
+
+
+CREATE VIEW cachorritas AS
+SELECT matricula, apellido
+FROM lista
+WHERE matricula==182483||matricula==179752||matricula==183060
+||matricula==181730||matricula==183213||matricula==182712
+||matricula==179800;
