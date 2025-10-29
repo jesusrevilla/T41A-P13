86c2f3b56784e9e77928ad030e566cc4775f618b
Uriel4443 <182829@upslp.edu.mx>
2025-10-28 19:52:02 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,3 +1,9 @@
+CREATE TABLE empleados (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100),
+    departamento VARCHAR(50),
+    salario NUMERIC
+);
 CREATE TABLE equipo (
     matricula VARCHAR(6) PRIMARY KEY,
     apellido VARCHAR(100) NOT NULL,
