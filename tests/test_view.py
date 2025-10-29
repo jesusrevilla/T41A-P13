e1b607454966377942187230c5cced7e11d392fe
tacodepastor25 <125671857+tacodepastor25@users.noreply.github.com>
2025-10-28 19:28:50 -0600

    Update test_view.py

--- tests/test_view.py
+++ tests/test_view.py
@@ -1,19 +1,19 @@
 import psycopg2
 
-def test_vista_ti():
+def test_vista_equipo():
     conn = psycopg2.connect(
-        dbname='test_db',
-        user='postgres',
-        password='postgres',
-        host='localhost',
-        port='5432'
+        dbname="test_db",
+        user="postgres",
+        password="postgres",
+        host="localhost",
+        port="5432"
     )
     cur = conn.cursor()
-    cur.execute(\"SELECT * FROM vista_ti;\")
-    resultados = cur.fetchall()
-    assert len(resultados) == 2  # Solo Luis y María están en TI
-    nombres = [fila[0] for fila in resultados]
-    assert 'Luis' in nombres
-    assert 'María' in nombres
+    cur.execute("SELECT * FROM vista_equipo;")
+    rows = cur.fetchall()
+
+    matriculas = [row[0] for row in rows]
+    assert "181662" in matriculas, "Tu matrícula no aparece en la vista_equipo"
+
     cur.close()
     conn.close()
