0bb37fd629351339c21c24e2596c5365253c2fcd
Uriel4443 <182829@upslp.edu.mx>
2025-10-28 21:01:47 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,11 +1,11 @@
+\copy equipo FROM 'lista.csv' DELIMITER ',' CSV HEADER;
+
+UPDATE equipo
+SET equi  = 1
+WHERE matricula IN ('182829', '182451', '182934', '179761');  
 INSERT INTO empleados (nombre, departamento, salario) VALUES
 ('Ana', 'Ventas', 50000),
 ('Luis', 'TI', 60000),
 ('María', 'TI', 65000),
 ('Carlos', 'Ventas', 55000),
 ('Elena', 'RRHH', 48000);
-\copy equipo FROM 'lista.csv' DELIMITER ',' CSV HEADER;
-
-UPDATE equipo
-SET equi  = 1
-WHERE matricula IN ('182829', '182451', '182934', '179761');  
