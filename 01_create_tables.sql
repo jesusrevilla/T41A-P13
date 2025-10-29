40c368bf7bf79a06e02fd004b55f4c692f8fdf6a
YaelQCI <181914@upslp.edu.mx>
2025-10-28 19:36:49 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -4,3 +4,20 @@ CREATE TABLE empleados (
     departamento VARCHAR(50),
     salario NUMERIC
 );
+
+CREATE TABLE alumnos(
+  matricula SERIAL PRIMARY KEY,
+  apellido TEXT NOT NULL
+);
+
+CREATE TABLE equipos(
+  id SERIAL PRIMARY KEY,
+  nombre TEXT NOT NULL
+);
+
+CREATE TABLE integrantes(
+  id_equipo INTEGER NOT NULL REFERENCES equipos(id),
+  matricula_alumno INTEGER NOT NULL REFERENCES alumnos(matricula),
+  PRIMARY KEY (id_equipo, matricula_alumno),
+  UNIQUE (matricula_alumno)
+);
