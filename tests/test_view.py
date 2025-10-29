44b35ae1d1be8cdf1a9abd734770589b8d6cc077
Uriel4443 <182829@upslp.edu.mx>
2025-10-28 20:26:11 -0600

    Update test_view.py

--- tests/test_view.py
+++ tests/test_view.py
@@ -17,3 +17,20 @@ def test_vista_ti():
     assert 'María' in nombres
     cur.close()
     conn.close()
+def test_integrante_equipo():
+    conn = psycopg2.connect(
+        dbname='test_db',
+        user='postgres',
+        password='postgres',
+        host='localhost',
+        port='5432'
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM integrante_equipo;")
+    resultados = cur.fetchall()
+    assert len(resultados) == 4 # Solo Luis y María están en TI
+    nombres = [fila[0] for fila in resultados]
+    assert 'García' in nombres
+    assert 'María' in nombres
+    cur.close()
+    conn.close()
