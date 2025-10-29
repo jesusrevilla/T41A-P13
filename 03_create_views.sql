e9430f8763e4bd166eb24d2436c48b3df98741e2
EDLR <160347985+ErikDlrr@users.noreply.github.com>
2025-10-28 19:21:44 -0600

    Update estudiantes and create vista_mi_equipo view

--- 03_create_views.sql
+++ 03_create_views.sql
@@ -1,3 +1,12 @@
 UPDATE estudiantes
 SET nombre_equipo = 'Consorcio Licenciado Valeriano'
 WHERE matricula IN ('177700', '177139');
+
+CREATE VIEW vista_mi_equipo AS
+SELECT
+    matricula,
+    apellido
+FROM
+    estudiantes
+WHERE
+    nombre_equipo = 'Consorcio Licenciado Valeriano';
