76a4cd3a539ea637a731c08e57ca7903c82f89c9
m00n07 <174653@upslp.edu.mx>
2025-10-29 18:57:07 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -4,3 +4,11 @@ INSERT INTO empleados (nombre, departamento, salario) VALUES
 ('María', 'TI', 65000),
 ('Carlos', 'Ventas', 55000),
 ('Elena', 'RRHH', 48000);
+
+\copy alumnos(matricula, apellido) FROM 'lista.csv' WITH (FORMAT CSV, HEADER TRUE);
+
+INSERT INTO equipos values (1,'EQU');
+
+INSERT INTO alumno_equipo values
+('174653',1),
+('179862',1);
