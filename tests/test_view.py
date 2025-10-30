7f9f00488f3c1c9dd2054cd573b97440d9e23a3d
m00n07 <174653@upslp.edu.mx>
2025-10-29 19:11:11 -0600

    Update test_view.py

--- tests/test_view.py
+++ tests/test_view.py
@@ -17,20 +17,3 @@ def test_vista_ti():
     assert 'María' in nombres
     cur.close()
     conn.close()
-
-def test_vista_equipo():
-    conn = psycopg2.connect(
-        dbname='test_db',
-        user='postgres',
-        password='postgres',
-        host='localhost',
-        port='5432'
-    )
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM vista_equipo;")
-    resultados = cur.fetchall()
-    matriculas = [fila[0] for fila in resultados]
-    assert '179862' in matriculas
-    assert '174653' in matriculas
-    cur.close()
-    conn.close()
