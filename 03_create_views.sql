34716cd992d4eb06a36ba2d27ea65638e3678daf
Diego0086 <178218@upslp.edu.mx>
2025-10-29 18:20:49 -0600

    Update 03_create_views.sql

--- 03_create_views.sql
+++ 03_create_views.sql
@@ -4,3 +4,10 @@ FROM empleados
 WHERE departamento = 'TI';
 
 SELECT * FROM vista_ti;
+
+CREATE VIEW integrantes AS
+SELECT matricula,nombre
+FROM equipos
+WHERE numero_equipo=5;
+
+SELECT * FROM integrantes;
