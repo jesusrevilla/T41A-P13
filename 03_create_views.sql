3646fd445159654bcaee7fedf5cc4b0cac777784
AngelCasti <125672277+AngelCasti@users.noreply.github.com>
2025-10-30 19:59:18 -0600

    Update 03_create_views.sql

--- 03_create_views.sql
+++ 03_create_views.sql
@@ -1,1 +1,9 @@
+CREATE VIEW empleadosVista AS
+SELECT *
+FROM empleados
+WHERE departamento = 'TI';
 
+CREATE VIEW equipoVista AS
+SELECT *
+FROM equipo
+WHERE matricula = '179752' OR matricula = '183213' OR matricula = '183060';
