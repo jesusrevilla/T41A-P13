7a451edd2bbc1ddd8563dcbf64407d9b86ef8f8a
VictorCastro135 <178920@upslp.edu.mx>
2025-11-02 18:53:49 -0600

    feature/178920

--- tests/test_view.py
+++ tests/test_view.py
@@ -17,3 +17,29 @@ def test_vista_ti():
     assert 'María' in nombres
     cur.close()
     conn.close()
+
+def test_vista_equipo6_por_matricula():
+    conn = psycopg2.connect(
+        dbname='test_db',
+        user='postgres',
+        password='postgres',
+        host='localhost',
+        port='5432'
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT matricula, apellido FROM vista_equipo6;")
+    resultados = cur.fetchall()
+    assert len(resultados) == 6
+    matriculas = [fila[0] for fila in resultados]
+    
+    assert '178920' in matriculas
+    assert '183016' in matriculas
+    assert '182570' in matriculas
+    assert '178584' in matriculas
+    assert '177888' in matriculas  
+    assert '175329' in matriculas
+
+    cur.close()
+    conn.close()
+
+
