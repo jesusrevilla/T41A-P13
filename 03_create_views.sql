8dc29efb83cc19df68f241938c9ce3fed369d65d
VictorCastro135 <178920@upslp.edu.mx>
2025-11-02 18:47:18 -0600

    feature/178920

--- 03_create_views.sql
+++ 03_create_views.sql
@@ -3,3 +3,11 @@ SELECT nombre, departamento, salario
 FROM empleados
 WHERE departamento = 'TI';
 
+CREATE VIEW vista_equipo6 AS
+SELECT 
+    a.matricula, 
+    a.apellido 
+FROM 
+    alumnos AS a
+JOIN 
+    equipo6 AS e ON a.matricula = e.matricula;
