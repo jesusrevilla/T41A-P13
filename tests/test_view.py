37a1275fc5414b8464d517b059d4c0bd636ac17a
SayraIbarra <130393897+SayraIbarra@users.noreply.github.com>
2025-11-04 01:21:24 -0600

    Update test_view.py

--- tests/test_view.py
+++ tests/test_view.py
@@ -1,6 +1,5 @@
 import psycopg2
-
-def test_vista_ti():
+def test_vista_equipo():
     conn = psycopg2.connect(
         dbname='test_db',
         user='postgres',
@@ -9,11 +8,24 @@ def test_vista_ti():
         port='5432'
     )
     cur = conn.cursor()
-    cur.execute(\"SELECT * FROM vista_ti;\")
+    cur.execute("SELECT * FROM vista_equipo;")
     resultados = cur.fetchall()
-    assert len(resultados) == 2  # Solo Luis y María están en TI
-    nombres = [fila[0] for fila in resultados]
-    assert 'Luis' in nombres
-    assert 'María' in nombres
+    # Extraer columnas
+    matriculas = [fila[0] for fila in resultados]
+    apellidos = [fila[1] for fila in resultados]
+    # Verificar que los tres compañeros del equipo están en la vista
+    assert 178561 in matriculas  # Palau
+    assert 176453 in matriculas  # Palau
+    assert 178666 in matriculas  # García
+    assert 178974 in matriculas  # Saucedo
+    assert 'Palau' in apellidos
+    assert 'Ibarra' in apellidos
+    assert 'García' in apellidos
+    assert 'Saucedo' in apellidos
+
+    # Verificar que solo hay tres registros
+    assert len(resultados) == 3
+    assert len(resultados) == 4
+
     cur.close()
     conn.close()
