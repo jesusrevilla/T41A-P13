08bbb4c321a72b118121830a67fe9944fd5381e9
SayraIbarra <130393897+SayraIbarra@users.noreply.github.com>
2025-11-04 15:27:43 -0600

    Update test_view.py

--- tests/test_view.py
+++ tests/test_view.py
@@ -9,11 +9,21 @@ def test_vista_ti():
         port='5432'
     )
     cur = conn.cursor()
-    cur.execute(\"SELECT * FROM vista_ti;\")
-    resultados = cur.fetchall()
-    assert len(resultados) == 2  # Solo Luis y María están en TI
-    nombres = [fila[0] for fila in resultados]
-    assert 'Luis' in nombres
-    assert 'María' in nombres
+    cur.execute('SELECT * FROM vista_ti;')
+   resultados = cur.fetchall()
+    # Extraer columnas
+    matriculas = [fila[0] for fila in resultados]
+    apellidos = [fila[1] for fila in resultados]
+    assert 178561 in matriculas  # Palau
+    assert 176453 in matriculas  # Ibarra
+    assert 178666 in matriculas  # García
+    assert 178974 in matriculas  # Saucedo
+    assert 'Palau' in apellidos
+    assert 'Ibarra' in apellidos
+    assert 'García' in apellidos
+    assert 'Saucedo' in apellidos
+
+    assert len(resultados) == 4
+
     cur.close()
     conn.close()
