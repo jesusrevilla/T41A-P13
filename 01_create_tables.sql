b5e74c7f1a950f78472414a984d533dfb53dc72e
andreupslpornelas <182085@upslp.edu.mx>
2025-11-03 19:19:03 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,1 +1,6 @@
-
+CREATE TABLE alumnos (
+    matricula VARCHAR(10) PRIMARY KEY, -- Matrícula como identificador único
+    nombre VARCHAR(100),
+    apellido VARCHAR(100),
+    nombre_equipo VARCHAR(50)
+);
