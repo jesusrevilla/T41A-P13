dd7d331e0e01af9f3ff483fedea5b7839cae28ad
tacodepastor25 <125671857+tacodepastor25@users.noreply.github.com>
2025-11-03 19:47:06 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,6 +1,14 @@
+-- Crear tabla empleados
 CREATE TABLE empleados (
     id SERIAL PRIMARY KEY,
     nombre VARCHAR(100),
     departamento VARCHAR(50),
     salario NUMERIC
+);
+
+-- Crear tabla compañeros
+CREATE TABLE companeros (
+    matricula INTEGER PRIMARY KEY,
+    apellido VARCHAR(100),
+    equipo VARCHAR(50)
 );
