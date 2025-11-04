d6d23f137d9e9f50f4f26b65adb2eee1b3f85e76
andreupslpornelas <182085@upslp.edu.mx>
2025-11-03 19:26:48 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,3 +1,17 @@
+-- Sentencias para el archivo: 01_create_tables.sql
+
+-- 1. Tabla de Empleados (del ejercicio práctico)
+DROP TABLE IF EXISTS empleados CASCADE;
+CREATE TABLE empleados (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100),
+    departamento VARCHAR(50),
+    salario NUMERIC
+);
+
+-- 2. Tabla de Alumnos/Equipo (del ejercicio 3)
+-- Esta tabla simula la carga de datos del archivo lista.csv
+DROP TABLE IF EXISTS alumnos CASCADE;
 CREATE TABLE alumnos (
     matricula VARCHAR(10) PRIMARY KEY, -- Matrícula como identificador único
     nombre VARCHAR(100),
