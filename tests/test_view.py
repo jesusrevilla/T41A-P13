26eb3d662dec70cce58f1cbdcb28c9c232a5b815
SayraIbarra <130393897+SayraIbarra@users.noreply.github.com>
2025-11-04 15:35:03 -0600

    Update test_view.py

--- tests/test_view.py
+++ tests/test_view.py
@@ -1,6 +1,5 @@
 import psycopg2
 def test_vista_ti():
-    # Asegúrate de que los parámetros de conexión coincidan con tu base de datos
     conn = psycopg2.connect(
         dbname='test_db',
         user='postgres',
@@ -9,7 +8,6 @@ def test_vista_ti():
         port='5432'
     )
     cur = conn.cursor()
-    # Sintaxis de la consulta corregida
     cur.execute('SELECT * FROM vista_ti;')
     
     resultados = cur.fetchall()
@@ -34,22 +32,20 @@ def test_vista_equipo():
         port='5432'
     )
     cur = conn.cursor()
-    # Sintaxis de la consulta corregida
+    
     cur.execute('SELECT * FROM vista_equipo;')
     
     resultados = cur.fetchall()
     
-    # Extraer columnas (asumiendo que matricula es el índice 0 y apellido es el índice 1)
     matriculas = [fila[0] for fila in resultados]
     apellidos = [fila[1] for fila in resultados]
     
-    # Matrículas de tu equipo (4 integrantes)
+  
     assert 178561 in matriculas  # Palau
     assert 176453 in matriculas  # Ibarra
     assert 178666 in matriculas  # García
     assert 178974 in matriculas  # Saucedo
     
-    # Apellidos de tu equipo
     assert 'Palau' in apellidos
     assert 'Ibarra' in apellidos
     assert 'García' in apellidos
