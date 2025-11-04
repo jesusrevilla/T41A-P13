d55ddc179c442c1717d7722256317624f9fc4b52
tacodepastor25 <125671857+tacodepastor25@users.noreply.github.com>
2025-11-03 19:48:28 -0600

    Update 03_create_views.sql

--- 03_create_views.sql
+++ 03_create_views.sql
@@ -1,6 +1,12 @@
-CREATE VIEW vista_ti AS 
-SELECT nombre
+-- Vista para empleados del departamento de TI
+CREATE VIEW vista_ti AS
+SELECT id, nombre, salario
 FROM empleados
 WHERE departamento = 'TI';
 
-SELECT * FROM vista_ti;
+-- Vista PRINCIPAL para tus compañeros del Equipo A
+CREATE VIEW mi_equipo AS
+SELECT matricula, apellido
+FROM companeros
+WHERE equipo = 'Equipo A'
+ORDER BY apellido;
