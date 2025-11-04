23bbb0648e8bbb73c06cdd06ffba48021077163f
andreupslpornelas <182085@upslp.edu.mx>
2025-11-03 19:19:58 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,1 +1,4 @@
-
+INSERT INTO alumnos (matricula, nombre, apellido, nombre_equipo) VALUES
+('A00123456', 'Juan', 'Pérez', 'Alfa'),
+('A00789012', 'Sofía', 'García', 'Alfa'),
+('A00345678', 'Pedro', 'López', 'Beta');
