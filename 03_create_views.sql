495d55394315a99524fbfcf0cd2442f992cd9e3e
Yaz Guerrero <yazmin.guerreroguev@gmail.com>
2025-11-03 19:35:29 -0600

    feature/182483

--- 03_create_views.sql
+++ 03_create_views.sql
@@ -2,3 +2,15 @@ CREATE VIEW vista_ti AS
 SELECT nombre
 FROM empleados
 WHERE departamento='TI';
+
+CREATE VIEW vista_equipo_yaz AS
+SELECT matricula, apellido FROM lista
+WHERE matricula IN (
+    '179752',
+    '177622',
+    '182483',
+    '182712',
+    '183060',
+    '179800',
+    '181730'
+);
