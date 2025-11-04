e901bc088b9588fcf9c1d939b5f205ac60b54659
SayraIbarra <130393897+SayraIbarra@users.noreply.github.com>
2025-11-04 01:19:26 -0600

    Update 03_create_views.sql

--- 03_create_views.sql
+++ 03_create_views.sql
@@ -1,1 +1,7 @@
-
+CREATE VIEW vista_equipo AS
+-- Crear vista solo con los compañeros del equipo 
+CREATE OR REPLACE VIEW vista_equipo AS
+SELECT matricula, apellido
+FROM companeros
+WHERE apellido IN ('Palau', 'Ibarra', 'García', 'Saucedo');
+WHERE matricula IN (178561, 176453, 178666, 178974);
