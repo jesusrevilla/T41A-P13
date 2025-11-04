de69921246a248d208bd65be11c32de067cdbf10
tacodepastor25 <125671857+tacodepastor25@users.noreply.github.com>
2025-11-03 19:50:20 -0600

    Update test_view.py

--- tests/test_view.py
+++ tests/test_view.py
@@ -12,7 +12,7 @@ def test_vista_ti():
     cur.execute("SELECT * FROM vista_ti;")
     resultados = cur.fetchall()
     assert len(resultados) == 2  # Solo Luis y María están en TI
-    nombres = [fila[0] for fila in resultados]
+    nombres = [fila[1] for fila in resultados]
     assert 'Luis' in nombres
     assert 'María' in nombres
     cur.close()
