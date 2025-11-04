d8326b2f634d9906e53b3576b34f52297ab48592
SayraIbarra <130393897+SayraIbarra@users.noreply.github.com>
2025-11-04 15:21:29 -0600

    Update 03_create_views.sql

--- 03_create_views.sql
+++ 03_create_views.sql
@@ -1,1 +1,15 @@
 
+CREATE VIEW vista_ti AS
+SELECT 
+    nombre, -- *** Poner 'nombre' como primera columna para que el test_view.py pase ***
+    id, 
+    departamento, 
+    salario
+FROM empleados
+WHERE departamento = 'TI';
+
+
+CREATE VIEW vista_equipo AS
+SELECT matricula, apellido
+FROM companeros
+WHERE equipo = 'T41A-P13'; 
