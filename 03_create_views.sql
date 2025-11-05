4b62cfb81a6332d7a3db4a4ab90319202b14d6b3
Fano-Rdz <178584@upslp.edu.mx>
2025-11-05 16:36:34 -0600

    Update 03_create_views.sql

--- 03_create_views.sql
+++ 03_create_views.sql
@@ -1,3 +1,12 @@
-CREATE VIEW vista_ti AS SELECT nombre 
-  FROM empleados 
-  WHERE departamento = 'TI';
+CREATE OR REPLACE VIEW vista_ti AS
+SELECT id, nombre, departamento, salario
+FROM empleados
+WHERE departamento = 'TI';
+
+
+CREATE OR REPLACE VIEW vista_equipo AS
+SELECT
+    matricula,
+    TRIM(nombres || ' ' || apellido_paterno || COALESCE(' ' || apellido_materno, '')) AS nombre_completo
+FROM companeros
+WHERE equipo = 'Equipo-JECEJ';
