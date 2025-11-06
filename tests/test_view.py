061824ac5f113ee9f87a43f725d76fdd308d56c7
daf01-web <171513@upslp.edu.mx>
2025-11-05 19:34:13 -0600

    Update test_view.py

--- tests/test_view.py
+++ tests/test_view.py
@@ -1,5 +1,5 @@
-import psycopg2
 
+import psycopg2
 def test_vista_ti():
     conn = psycopg2.connect(
         dbname='test_db',
@@ -10,6 +10,7 @@ def test_vista_ti():
     )
     cur = conn.cursor()
     cur.execute(\"SELECT * FROM vista_ti;\")
+    cur.execute("SELECT * FROM vista_ti;")
     resultados = cur.fetchall()
     assert len(resultados) == 2  # Solo Luis y María están en TI
     nombres = [fila[0] for fila in resultados]
@@ -17,3 +18,4 @@ def test_vista_ti():
     assert 'María' in nombres
     cur.close()
     conn.close()
+    
