529986ed0f79329a30f730739f4701f8cd4d55e2
daf01-web <171513@upslp.edu.mx>
2025-11-10 12:08:26 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,5 +1,7 @@
 
-CREATE VIEW nombre_vista AS
-SELECT columnas
-FROM tabla
-WHERE condiciones;
+CREATE TABLE empleados (
+  id SERIAL PRIMARY KEY,
+  nombre VARCHAR(100),
+  departamento VARCHAR(50),
+  salario NUMERIC
+);
