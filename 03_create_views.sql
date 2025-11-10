ef736a64f1cf0a541ca29aa919192e849fed5f3f
daf01-web <171513@upslp.edu.mx>
2025-11-10 11:34:05 -0600

    Update 03_create_views.sql

--- 03_create_views.sql
+++ 03_create_views.sql
@@ -3,12 +3,3 @@ CREATE VIEW vista_ti AS
 SELECT nombre
 FROM empleados
 WHERE departamento = 'TI';
-
-SELECT * FROM vista_ti;
-
-CREATE VIEW integrantes AS
-SELECT matricula,nombre
-FROM equipos
-WHERE numero_equipo=5;
-
-SELECT * FROM integrantes;
