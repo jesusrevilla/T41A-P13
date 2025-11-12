c85226c4844d2b1bdacdcad810b7fced2fb4363b
Javierxd1383 <javiercruzz1383@gmail.com>
2025-11-11 19:20:56 -0600

    Update 03_create_views.sql

--- 03_create_views.sql
+++ 03_create_views.sql
@@ -1,12 +1,10 @@
 
+CREATE OR REPLACE VIEW vista_cachorritas AS
+SELECT matricula, apellido
+FROM lista
+WHERE equipo = 'Cachorritas';
+
 CREATE OR REPLACE VIEW vista_ti AS
-SELECT id, nombre, departamento, salario
+SELECT nombre, departamento, salario
 FROM empleados
 WHERE departamento = 'TI';
-
-CREATE OR REPLACE VIEW vista_mi_equipo AS
-SELECT c.matricula, c.apellido
-FROM companeros c
-JOIN equipos e ON c.equipo_id = e.equipo_id
-WHERE e.equipo_nombre = 'MiEquipo';
-
