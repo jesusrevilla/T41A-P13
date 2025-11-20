37ac2a3b7d4c166c78c66bbf51f9fe61c5722629
Javierxd1383 <javiercruzz1383@gmail.com>
2025-11-19 18:20:16 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,20 +1,8 @@
-
-\copy lista_temp FROM 'lista.csv' DELIMITER ',' CSV HEADER;
-
-INSERT INTO lista (matricula, apellido, equipo)
-SELECT 
-    matricula,
-    apellido,
-    CASE 
-        WHEN matricula IN (179800,181730,179752,183060,182483,178974,177622)
-        THEN 'Cachorritas'
-        ELSE 'Otros'
-    END AS equipo
-FROM lista_temp;
-
 INSERT INTO empleados (nombre, departamento, salario) VALUES
+('Ana', 'Ventas', 50000),
 ('Luis', 'TI', 60000),
 ('María', 'TI', 65000),
-('Ana', 'Ventas', 50000),
 ('Carlos', 'Ventas', 55000),
 ('Elena', 'RRHH', 48000);
+
+\copy lista(matricula, apellido) FROM 'lista.csv' DELIMITER ',' CSV HEADER;
