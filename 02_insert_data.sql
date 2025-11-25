29b5e348e45587f430debb9caa0d8ba74ba9f54d
EdgarErh <160251219+EdgarErh@users.noreply.github.com>
2025-11-25 13:46:13 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,8 +1,8 @@
 INSERT INTO empleados (nombre, departamento, salario) VALUES
-('Edgar', 'Ventas', 50000),
-('Carlos', 'Piso', 60000),
+('Ana', 'Ventas', 50000),
+('Luis', 'TI', 60000),
 ('María', 'TI', 65000),
-('Marco', 'Ventas', 55000),
+('Carlos', 'Ventas', 55000),
 ('Elena', 'RRHH', 48000);
 
 \copy equipo FROM 'lista.csv' DELIMITER ',' CSV HEADER;
