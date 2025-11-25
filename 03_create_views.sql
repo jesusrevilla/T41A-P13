f4bad95af243515650a53da3e47e05c61ab55f70
EdgarErh <160251219+EdgarErh@users.noreply.github.com>
2025-11-25 13:46:48 -0600

    Update 03_create_views.sql

--- 03_create_views.sql
+++ 03_create_views.sql
@@ -1,1 +1,9 @@
+CREATE VIEW vista_ti AS
+SELECT nombre, departamento
+FROM empleados
+WHERE departamento = 'TI';
 
+CREATE VIEW equipoVista AS
+SELECT *
+FROM equipo
+WHERE matricula = '177888' OR matricula = '183213' OR matricula = '183060';
