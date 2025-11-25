ad38592b5bfc5d303c261c68be81fbb142da2990
EdgarErh <160251219+EdgarErh@users.noreply.github.com>
2025-11-25 13:36:19 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,1 +1,11 @@
+CREATE TABLE empleados (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100),
+    departamento VARCHAR(50),
+    salario NUMERIC
+);
 
+CREATE TABLE equipo (
+  matricula INT PRIMARY KEY,
+  apellido TEXT NOT NULL
+);
