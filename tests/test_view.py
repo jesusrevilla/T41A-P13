cd35d026ff94a71813efc48c73bd2ddc582d9dde
EdgarErh <160251219+EdgarErh@users.noreply.github.com>
2025-11-25 13:52:05 -0600

    Update test_view.py

--- tests/test_view.py
+++ tests/test_view.py
@@ -1,5 +1,4 @@
 import psycopg2
-
 def test_vista_ti():
     conn = psycopg2.connect(
         dbname='test_db',
@@ -10,6 +9,7 @@ def test_vista_ti():
     )
     cur = conn.cursor()
     cur.execute(\"SELECT * FROM vista_ti;\")
+    cur.execute("SELECT * FROM vista_ti;")
     resultados = cur.fetchall()
     assert len(resultados) == 2  # Solo Luis y María están en TI
     nombres = [fila[0] for fila in resultados]
